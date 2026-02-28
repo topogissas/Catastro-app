20753c82cb5419316d57c49a1667bb225cbbce2a
Javier Topogis <javier@topogis.co>
2026-02-28 16:08:51 -0500

    Initial commit: Catastro SECTOR_1_1 app

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'catastro-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'data/features.json',
+  'manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
